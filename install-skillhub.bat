0a4e60517397a8269aea3f93ccfb3fc1c68caadf
DESKTOP-94PGQME\Administrator <liuyiting120@126.com>
2026-03-28 17:37:51 +0800

    Remove unused scripts

--- install-skillhub.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Installing SkillHub...
-npm cache clean --force
-npm install -g skillhub
-echo.
-echo Done. Press any key to close.
-pause >nul
--- install_sharp.ps1
+++
@@ -1,5 +0,0 @@
-$ErrorActionPreference = "Stop"
-$dest = "D:\Program Files\QClaw\resources\openclaw\node_modules\openclaw"
-Write-Host "Installing sharp in: $dest"
-npm install sharp --save --prefix $dest 2>&1
-Write-Host "Done. Exit code: $LASTEXITCODE"
